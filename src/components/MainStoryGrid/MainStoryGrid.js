486c9e2103bf01eea30b4da549a567363e08ade2
Brenna Moore <brenna.moore@vista.co>
2023-05-23 09:31:34 +1200

    Exercise 3: Main story grid

--- src/components/MainStoryGrid/MainStoryGrid.js
+++ src/components/MainStoryGrid/MainStoryGrid.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components/macro";
 
 import { MAIN_STORY, OPINION_STORIES, SECONDARY_STORIES } from "../../data";
-import { COLORS } from "../../constants";
+import { COLORS, QUERIES } from "../../constants";
 
 import SectionTitle from "../SectionTitle";
 import MainStory from "../MainStory";
@@ -56,25 +56,38 @@ const Wrapper = styled.div`
     "advertisement";
   gap: 48px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    grid-template-columns: 1fr 252px;
+    grid-template-areas: "main-story secondary-stories" "advertisement advertisement" "opinion-stories opinion-stories";
+    column-gap: 0;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-columns: 1fr 419px 273px;
+    grid-template-areas: "main-story secondary-stories opinion-stories" "main-story advertisement advertisement";
+    gap: 0;
+  }
 `;
 
 const MainStorySection = styled.section`
   grid-area: main-story;
+
+  @media ${QUERIES.tabletAndUp} {
+    border-right: 1px solid ${COLORS.gray[300]};
+    padding-right: 16px;
+    margin-right: 16px;
+  }
 `;
 
 const SecondaryStorySection = styled.section`
   grid-area: secondary-stories;
-`;
 
-const Divider = styled.hr`
-  border: 1px solid ${COLORS.gray[300]};
-  width: 100%;
-  margin: 12px 0 16px;
-`;
-
-const StoryList = styled.div`
-  display: flex;
-  flex-direction: column;
+  @media ${QUERIES.laptopAndUp} {
+    border-right: 1px solid ${COLORS.gray[300]};
+    padding-right: 16px;
+    margin-right: 16px;
+  }
 `;
 
 const OpinionSection = styled.section`
@@ -83,6 +96,37 @@ const OpinionSection = styled.section`
 
 const AdvertisementSection = styled.section`
   grid-area: advertisement;
+
+  @media ${QUERIES.laptopAndUp} {
+    border-top: 1px solid ${COLORS.gray[300]};
+    padding-top: 16px;
+    margin-top: 16px;
+  }
+`;
+
+const StoryList = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  ${OpinionSection} & {
+    @media ${QUERIES.tabletOnly} {
+      flex-direction: row;
+      gap: 32px;
+    }
+  }
+`;
+
+const Divider = styled.hr`
+  border: none;
+  border-bottom: 1px solid ${COLORS.gray[300]};
+  width: 100%;
+  margin: 12px 0 16px;
+
+  ${OpinionSection} & {
+    @media ${QUERIES.tabletOnly} {
+      display: none;
+    }
+  }
 `;
 
 export default MainStoryGrid;
